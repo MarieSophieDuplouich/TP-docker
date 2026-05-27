13d671985b45055c6139da839bf51dd375f3f960
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2026-05-27 14:03:06 +0200

    Cours initial fait Faire le Allez plus loin : Variable d'envrionnement, DNS et hostname dans Docker Compose

---
+++ Tp-3/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$DATABASE_HOST = "bdd";
+$DATABASE_NAME = "ma-bdd";
+$DATABASE_USER = "root";
+$DATABASE_PASS = "root";
+$bdd = new PDO("mysql:host=$DATABASE_HOST;dbname=$DATABASE_NAME",$DATABASE_USER,$DATABASE_PASS);
+var_dump($bdd);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Test de connexion à la base de données</h1>
+  <p>Si vous voyez un objet PDO, la connexion à la base de données a réussi.</p>
+</body>
+</html>
